dcb1a3290808a036065cb5a0fd1b35775bb267f6
yahya567 <yahiyamohammed455#gmail.com>
2022-06-01 02:54:34 +0300

    okay?

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+    for i in range(0, 101):
+        if i % 3 == 0 and i % 5 != 0:
+            dis = 'Fizz'
+            print('{} '.format(dis), end="")
+        elif i % 5 == 0 and i % 3 != 0:
+            dis = 'Buzz'
+            print('{} '.format(dis), end="")
+        elif i % 5 == 0 and i % 3 == 0:
+            dis = 'FizzBuzz'
+            print('{} '.format(dis), end="")
+        else:
+            dis = i
+            print('{} '.format(dis), end="")
