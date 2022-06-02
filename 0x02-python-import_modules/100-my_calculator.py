fc2d1b180a9a7e100d9d8dc7d2b6cdca29515e89
yahya567 <yahiyamohammed455#gmail.com>
2022-06-03 00:17:31 +0300

    Hot fix

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
-from sys import argv, exit
-from calculator_1 import add, sub, mul, div
-
-
 if __name__ == "__main__":
-    arglen = len(argv) - 1
-    if arglen == 0:
-        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+    import sys
+    from calculator_1 import add, sub, mul, div
+    argv = sys.argv[1:]
+    argv_count = len(argv)
+    operators = ["+", "-", "*", "/"]
+    if argv_count is not 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    op = argv[2]
-    a, b = int(argv[1]), int(argv[3])
-    if op == "+":
-        print('{} + {} = {}'.format(a, b, add(a, b)))
-    elif op == "-":
-        print('{} - {} = {}'.format(a, b, sub(a, b)))
-    elif op == "*":
-        print('{} * {} = {}'.format(a, b, mul(a, b)))
-    elif op == "/":
-        print('{} / {} = {}'.format(a, b, div(a, b)))
+    elif sys.argv[2] not in operators:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
     else:
-        print('Unknown operator. Available operators: +, -, * and /')
-        exit(1)
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        if sys.argv[2] is "+":
+            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+        elif sys.argv[2] is "-":
+            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+        elif sys.argv[2] is "*":
+            print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+        elif sys.argv[2] is "/":
+            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,3 +1,2 @@
 #!/usr/bin/python3
-import sys
-sys.stdout.write("#pythoniscool")
+exec('import subprocess; subprocess.call(["echo", "#pythoniscool"])')
