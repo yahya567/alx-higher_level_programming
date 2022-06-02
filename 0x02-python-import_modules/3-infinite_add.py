2dd1c30e326173d51a70588d9c275f34ee5e6559
yahya567 <yahiyamohammed455#gmail.com>
2022-06-02 23:52:13 +0300

    www

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,6 +4,6 @@ from sys import argv
 if __name__ == "__name__":
     s = 0
     if len(argv) > 1:
-        for i in range(len(argv)):
+        for i in range(1, len(argv)):
                 s += int(argv[i])
     print('{}'.format(s))
